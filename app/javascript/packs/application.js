9508afc127024316c3c697f3ffa1708a2ef79a68
Koshi.Michisaka <20223519+michisaka@users.noreply.github.com>
2020-02-02 13:57:24 +0900

    静的ページを作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+require("jquery")
+// require("bootstrap")
+require("bootstrap-material-design")
+require("@fortawesome/fontawesome-free/js/all")
+
+import '../stylesheet/application.scss';
